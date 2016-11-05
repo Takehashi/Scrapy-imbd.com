4a29b3d7b7f8bf902ae71c7e8fc17eccdd0068ef
Takehashi <tranleanhthe@gmail.com>
2016-11-05 09:43:51 +0700

    update save in item

--- spider.py
+++ spider.py
@@ -28,13 +28,22 @@ class ImbdSpiderSpider(scrapy.Spider):
       	i = 0
       	for block in tree:
       		#print table	
-      		title = block.xpath('//h4[@itemprop="name"]/a/text()').extract()
-      		# author = block.xpath('//span[@itemprop="director"]/span/a/text()')[i].extract()
-      		# rate = block.xpath('//div[@class="metascore no_ratings"]/strong/text()')[i].extract()
-      		# time = block.xpath('//time[@itemprop="duration"]/text()')[i].extract()
-      		# tag = block.xpath('//span[@itemprop="genre"]/text()').extract()
-      		# des = block.xpath('//div[@class="outline"]/text()')[i].extract()
-      		print title
+      		title = block.xpath('.//h4[@itemprop="name"]/a/text()').extract()
+      		author = block.xpath('.//span[@itemprop="director"]/span/a/text()')[i].extract()
+      		rate = block.xpath('.//div[@class="metascore no_ratings"]/strong/text()')[i].extract()
+      		time = block.xpath('.//time[@itemprop="duration"]/text()')[i].extract()
+      		tag = block.xpath('.//span[@itemprop="genre"]/text()').extract()
+      		des = block.xpath('.//div[@class="outline"]/text()')[i].extract()
+		
+		item = ScrapyIdbm(scrapy.item)
+		item('title') = title
+		item('author') = author
+		item('rate') = rate
+		item('time') = time
+		item('tag') = tag
+		item('des') = des
+		yield item
+      		# print title
       		# print author
       		# print rate
       		# print time
@@ -42,11 +51,10 @@ class ImbdSpiderSpider(scrapy.Spider):
       		# print des
       		#i = i + 1
       
-       # 	page = response.xpath('//div[@class="sort"]/a')
-       # 	page2 = page.xpath('//a[text()="Next"]/@href')
-       #  nextpage = "http://www.imdb.com" + page2[0].extract() # /movie-coming-soon..
-
-      	# yield scrapy.Request(nextpage, self.parse)
+       page = response.xpath('//div[@class="sort"]/a')
+       page2 = page.xpath('//a[text()="Next"]/@href')
+       nextpage = "http://www.imdb.com" + page2[0].extract() # /movie-coming-soon.
+       yield scrapy.Request(nextpage, self.parse)
 
     def start_requests(self):
     	start = "http://www.imdb.com/movies-coming-soon/2017-12/"
